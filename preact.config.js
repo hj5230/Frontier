04ff6f29304a6e730d640705f596f3c8c8b2a957
zhaoshangqi <zhaoshangqi@baidu.com>
2024-07-20 21:32:36 +0800

    chore: configure alias

---
+++ preact.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import path from 'node:path'
+
+export default config => {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@assets': path.resolve(__dirname, 'src/assets'),
+    '@components': path.resolve(
+      __dirname,
+      'src/components',
+    ),
+    '@routes': path.resolve(__dirname, 'src/routes'),
+    '@services': path.resolve(__dirname, 'src/services'),
+    '@style': path.resolve(__dirname, 'src/style'),
+  }
+
+  return config
+}
