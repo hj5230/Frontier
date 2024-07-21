f9f2455eda54ee16c24a48796c373b4ef7e77763
zhaoshangqi <zhaoshangqi@baidu.com>
2024-07-21 21:59:21 +0800

    feat: use preact redux

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,27 @@
 /* eslint-disable no-undef */
-import path from 'path'
+import path, { resolve } from 'path'
+import webpack from 'webpack'
+import dotenv from 'dotenv'
 
 export default config => {
+  const envVars = dotenv.config({
+    path: resolve(__dirname, '.env'),
+    encoding: 'utf8',
+    debug: false,
+  }).parsed
+
+  const envKeys = Object.keys(envVars || {}).reduce(
+    (prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(
+        envVars[next],
+      )
+      return prev
+    },
+    {},
+  )
+
+  config.plugins.push(new webpack.DefinePlugin(envKeys))
+
   config.resolve.alias = {
     ...config.resolve.alias,
     '@assets': path.resolve(__dirname, 'src/assets'),
@@ -15,6 +35,7 @@ export default config => {
     ),
     '@routes': path.resolve(__dirname, 'src/routes'),
     '@services': path.resolve(__dirname, 'src/services'),
+    '@store': path.resolve(__dirname, 'src/store'),
     '@style': path.resolve(__dirname, 'src/style'),
   }
 
