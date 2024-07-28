ad7331bf461893c8f9ebca05e8e28e22b6c1a31c
hj5230 <hf305@foxmail.com>
2024-07-28 13:43:09 +0800

    feat: optimize bundle process

--- preact.config.js
+++ preact.config.js
@@ -1,9 +1,19 @@
 /* eslint-disable no-undef */
 import path from 'path'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export default config => {
+export default (config, env) => {
   config.resolve.alias = {
+    // original alias
     ...config.resolve.alias,
+
+    // react => preact alias
+    react: 'preact/compat',
+    'react-dom': 'preact/compat',
+    'react-dom/test-utils': 'preact/test-utils',
+    'react/jsx-runtime': 'preact/jsx-runtime',
+
+    // @path alias
     '@assets': path.resolve(__dirname, 'src/assets'),
     '@components': path.resolve(
       __dirname,
@@ -16,6 +26,22 @@ export default config => {
     '@routes': path.resolve(__dirname, 'src/routes'),
     '@services': path.resolve(__dirname, 'src/services'),
     '@style': path.resolve(__dirname, 'src/style'),
+  }
+
+  // we are very clear that its packaging volume exceeds the limit
+  config.performance = {
+    hints: false,
+  }
+
+  // create bundle analyzer when production build
+  if (env.production) {
+    console.warn('Building in Production Mode...')
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+      }),
+    )
   }
 
   return config
