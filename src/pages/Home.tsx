f449385061fcca89cdac13909d1b42f56dfe0807
hj5230 <hf305@foxmail.com>
2024-07-28 16:28:59 +0800

    feat: create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { h, VNode } from 'preact'
+
+import Typewriter from '@components/TypeWriter'
+
+const Home = (): VNode => {
+  return <Typewriter text="Hello World" />
+}
+
+export default Home
