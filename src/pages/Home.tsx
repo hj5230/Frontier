aca8685416f1faa8924f3b2bcf1d304cc30cd8ad
hj5230 <hf305@foxmail.com>
2024-07-28 18:03:01 +0800

    chore: remove template files

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { h, VNode } from 'preact'
-
-import Typewriter from '@components/TypeWriter'
-
-const Home = (): VNode => {
-  return <Typewriter text="Hello World" />
-}
-
-export default Home
