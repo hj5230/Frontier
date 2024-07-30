275043c4f9c80a34df91006842ca8b3bfe5b3f81
hj5230 <hf305@foxmail.com>
2024-07-30 23:38:40 +0800

    feat: highlight current path link

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import { h, FunctionComponent, VNode } from 'preact'
-import { useState } from 'preact/hooks'
+import { useState, useEffect } from 'preact/hooks'
 
 import {
   Avatar,
@@ -17,38 +17,68 @@ import {
   MagnifyingGlassIcon,
 } from '@radix-ui/react-icons'
 
-import definition from '@assets/definition'
+import navbar_definition from '@assets/definition.navbar'
 
 /**
  * @param _description_ TODO: avatar src should from api
  */
 const Navbar: FunctionComponent = (): VNode => {
   const [isOpen, setIsOpen] = useState(false)
+  const [currentPath, setCurrentPath] = useState(
+    window.location.pathname,
+  )
+
+  useEffect(() => {
+    const handleLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    window.addEventListener(
+      'popstate',
+      handleLocationChange,
+    )
+    return () => {
+      window.removeEventListener(
+        'popstate',
+        handleLocationChange,
+      )
+    }
+  }, [])
+
+  const handleLinkClick = (path: string) => {
+    setCurrentPath(path)
+  }
 
   return (
     <Box py="1" px="1">
       <Flex justify="between" align="center">
         <Flex align="center" gap="4">
-          <Link href="/">
+          <Link
+            href="/"
+            onClick={() => handleLinkClick('/')}
+          >
             <Avatar
-              src={definition.site_icon_uri}
+              src={navbar_definition.site_icon_uri}
               fallback="SZ"
               size="4"
             />
           </Link>
           <Separator orientation="vertical" size="2" />
-          <Link href="/" weight="bold" size="3">
-            Home
-          </Link>
-          <Link href="/resume" weight="bold" size="3">
-            Resume
-          </Link>
-          <Link href="/projects" weight="bold" size="3">
-            Project
-          </Link>
-          <Link href="/contact" weight="bold" size="3">
-            Contact
-          </Link>
+          {navbar_definition.navigator_items.map(n => (
+            <Link
+              key={n.path}
+              href={n.path}
+              weight="bold"
+              size="4"
+              style={{ margin: '10px' }}
+              underline={
+                currentPath === n.path ? 'always' : 'hover'
+              }
+              highContrast={currentPath !== n.path}
+              onClick={() => handleLinkClick(n.path)}
+            >
+              {n.name}
+            </Link>
+          ))}
         </Flex>
         <Flex align="center" gap="4">
           <TextField.Root
