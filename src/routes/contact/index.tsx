40691fb9cdc0a3db1ed94a4db1d3f61b2f964b13
hj5230 <hf305@foxmail.com>
2024-08-02 21:22:55 +0800

    feat: build skeleton for project & contact page

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,10 +1,18 @@
 import { h, FunctionComponent, VNode } from 'preact'
 
+import { Container } from '@radix-ui/themes'
+
+import GlowPanel from '@components/GlowPanel'
+
 const Contact: FunctionComponent = (): VNode => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <Container
+      size="4"
+      py="6"
+      style={{ maxWidth: '90%', margin: '0 auto' }}
+    >
+      <GlowPanel>Contact</GlowPanel>
+    </Container>
   )
 }
 
--- src/routes/project/index.tsx
+++ src/routes/project/index.tsx
@@ -1,10 +1,18 @@
 import { h, FunctionComponent, VNode } from 'preact'
 
+import { Container } from '@radix-ui/themes'
+
+import GlowPanel from '@components/GlowPanel'
+
 const Project: FunctionComponent = (): VNode => {
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <Container
+      size="4"
+      py="6"
+      style={{ maxWidth: '90%', margin: '0 auto' }}
+    >
+      <GlowPanel>Project</GlowPanel>
+    </Container>
   )
 }
 
