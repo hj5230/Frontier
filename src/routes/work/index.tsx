1002b27bd2aae16183ee9f4417d3d5fafe4e5e4d
hj5230 <hf305@foxmail.com>
2024-08-06 03:13:53 +0800

    feat: complete work page

--- src/routes/work/index.tsx
+++ src/routes/work/index.tsx
@@ -1,12 +1,69 @@
 import { h, FunctionComponent, VNode } from 'preact'
 
+import {
+  Flex,
+  Heading,
+  Text,
+  Blockquote,
+  Badge,
+  Strong,
+  Code,
+} from '@radix-ui/themes'
+
 import GlowPanel from '@components/GlowPanel'
+import Typewriter from '@components/TypeWriter'
+
+import work_definition from '@assets/definition.work'
 
 const Work: FunctionComponent = (): VNode => {
   return (
-    <GlowPanel>
-      <h1>Work</h1>
-    </GlowPanel>
+    <Flex direction="column" gap="3">
+      {work_definition.work.map(w => {
+        const children = (
+          <Flex direction="column" gap="2">
+            <Flex justify="between">
+              <Heading size="6">{w.company}</Heading>
+              <Flex justify="between" gap="4">
+                <Flex gap="2">
+                  {w.keywords &&
+                    w.keywords.map(k => (
+                      <Badge color={k.color}>
+                        {k.text}
+                      </Badge>
+                    ))}
+                </Flex>
+                <Text as="p" color="gray" size="2">
+                  {w.period}
+                </Text>
+              </Flex>
+            </Flex>
+            <Text size="4">
+              <Flex gap="4">
+                <Strong>{w.department}</Strong>
+                <Code>{w.role}</Code>
+              </Flex>
+            </Text>
+            {w.description.map(d => (
+              <Blockquote>
+                <Typewriter text={d} />
+              </Blockquote>
+            ))}
+          </Flex>
+        )
+
+        if (w.themeColor)
+          return (
+            <GlowPanel
+              inputStyle={{
+                border: `1px solid ${w.themeColor}`,
+              }}
+            >
+              {children}
+            </GlowPanel>
+          )
+        else return <GlowPanel>{children}</GlowPanel>
+      })}
+    </Flex>
   )
 }
 
