65cb251e345e71e438e90f582bac8dd85db8ffea
hj5230 <hf305@foxmail.com>
2024-08-24 16:24:51 +0800

    Revert "feat: implement hook useIsMobile"
    This reverts commit 6b7bddc5440a7234d2e78f32465d0a7119aa8574.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useLocation } from '@hooks/useLocation'
 export { useNotification } from '@hooks/useNotification'
 export { useVisitorInfo } from '@hooks/useVisitorInfo'
-export { useIsMobile } from '@hooks/useIsMobile'
