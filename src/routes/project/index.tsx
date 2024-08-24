85e00a62dd30cd160ca428065ef9fa5b3180354e
hj5230 <hf305@foxmail.com>
2024-08-24 17:02:24 +0800

    chore: temporarily comment out unreachables

--- src/routes/project/index.tsx
+++ src/routes/project/index.tsx
@@ -1,15 +1,15 @@
 import { h, FunctionComponent, VNode } from 'preact'
 
-import { Badge } from '@themes/badge'
-import { Flex } from '@themes/flex'
 import { Heading } from '@themes/heading'
-import { Text } from '@themes/text'
+// import { Badge } from '@themes/badge'
+// import { Flex } from '@themes/flex'
+// import { Text } from '@themes/text'
 
-import { GlowPanel } from '@components/GlowPanel'
-import { Media } from '@components/Media'
+// import { GlowPanel } from '@components/GlowPanel'
+// import { Media } from '@components/Media'
 
-import project_definition from '@assets/definition.project'
-import { Typewriter } from '@components/TypeWriter'
+// import project_definition from '@assets/definition.project'
+// import { Typewriter } from '@components/TypeWriter'
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
 
 // TODO: move Container to app.tsx for all routes
@@ -23,45 +23,45 @@ const Project: FunctionComponent = (): VNode => {
     </div>
   )
 
-  return (
-    <Flex direction="column" gap="3">
-      {project_definition.project.map(p => {
-        const children = (
-          <Flex direction="column">
-            <Flex justify="between">
-              <Heading>{p.name}</Heading>
-              <Flex>
-                {p.keywords &&
-                  p.keywords.map(k => (
-                    <Badge color={k.color}>{k.text}</Badge>
-                  ))}
-              </Flex>
-              <Text as="p" color="gray" size="2">
-                {p.period}
-              </Text>
-            </Flex>
-            <Text>{p.description}</Text>
-            <Media type="video" uri={p.media_uri} />
-            <Typewriter text={p.comment} />
-          </Flex>
-        )
+  // return (
+  //   <Flex direction="column" gap="3">
+  //     {project_definition.project.map(p => {
+  //       const children = (
+  //         <Flex direction="column">
+  //           <Flex justify="between">
+  //             <Heading>{p.name}</Heading>
+  //             <Flex>
+  //               {p.keywords &&
+  //                 p.keywords.map(k => (
+  //                   <Badge color={k.color}>{k.text}</Badge>
+  //                 ))}
+  //             </Flex>
+  //             <Text as="p" color="gray" size="2">
+  //               {p.period}
+  //             </Text>
+  //           </Flex>
+  //           <Text>{p.description}</Text>
+  //           <Media type="video" uri={p.media_uri} />
+  //           <Typewriter text={p.comment} />
+  //         </Flex>
+  //       )
 
-        if (p.themeColor) {
-          return (
-            <GlowPanel
-              inputStyle={{
-                border: `1px solid ${p.themeColor}`,
-              }}
-            >
-              {children}
-            </GlowPanel>
-          )
-        } else {
-          return <GlowPanel>{children}</GlowPanel>
-        }
-      })}
-    </Flex>
-  )
+  //       if (p.themeColor) {
+  //         return (
+  //           <GlowPanel
+  //             inputStyle={{
+  //               border: `1px solid ${p.themeColor}`,
+  //             }}
+  //           >
+  //             {children}
+  //           </GlowPanel>
+  //         )
+  //       } else {
+  //         return <GlowPanel>{children}</GlowPanel>
+  //       }
+  //     })}
+  //   </Flex>
+  // )
 }
 
 export default Project
