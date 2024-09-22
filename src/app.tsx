1da4ccbcf5264aeeff8d62119d239fa0490c093c
hj5230 <hf305@foxmail.com>
2024-09-22 08:51:55 +0800

    refactor: deprecate local static definitions with useDefinitions hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,18 @@
 import { h, FunctionComponent, VNode } from 'preact'
-import { useState } from 'preact/hooks'
+
 import { Route, Router } from 'preact-router'
 
+import { useState } from 'preact/hooks'
+import { useDefinitions } from '@hooks/.'
+
 import { Appearance } from '@typings/appearance'
+import { DefinitionModule } from '@typings/definition'
 
 import { Theme } from '@themes/theme'
 import { Separator } from '@themes/separator'
+import { Skeleton } from '@themes/skeleton'
+import { Flex } from '@themes/flex'
+import { Box } from '@themes/box'
 
 import { Draggable } from '@components/Draggable'
 import { Navbar } from '@components/Navbar'
@@ -18,8 +25,6 @@ import Project from '@routes/project'
 import Contact from '@routes/contact'
 import Work from '@routes/work'
 import $error from '@routes/error'
-
-import app_definition from '@assets/definition.app'
 
 const App: FunctionComponent = (): VNode => {
   const [appearance, setAppearance] = useState(
@@ -34,10 +39,14 @@ const App: FunctionComponent = (): VNode => {
     )
   }
 
-  return (
+  const [definitions, loading, error] = useDefinitions(
+    DefinitionModule.APP,
+  )
+
+  const layout = (content: VNode) => (
     <Theme
       appearance={appearance}
-      accentColor="iris" // by defualt
+      accentColor="iris"
       grayColor="sage"
       style={{ minHeight: 0 }}
     >
@@ -57,31 +66,68 @@ const App: FunctionComponent = (): VNode => {
           height: 'calc(100vh - 100px)',
         }}
       >
-        <Router>
-          <Route
-            path={app_definition.path[0].path}
-            component={Home}
-          />
-          <Route
-            path={app_definition.path[1].path}
-            component={Resume}
-          />
-          <Route
-            path={app_definition.path[2].path}
-            component={Project}
-          />
-          <Route
-            path={app_definition.path[3].path}
-            component={Work}
-          />
-          <Route
-            path={app_definition.path[4].path}
-            component={Contact}
-          />
-          <Route default component={$error} />
-        </Router>
+        {content}
       </Panel>
     </Theme>
+  )
+
+  if (loading) {
+    return layout(
+      <Flex gap="4">
+        <Skeleton>
+          <Box width="160px" height="160px" />
+        </Skeleton>
+        <Flex direction="column" gap="2">
+          <Skeleton>
+            <Box width="120px" height="40px" />
+          </Skeleton>
+          <Skeleton>
+            <Box width="80px" height="30px" />
+          </Skeleton>
+        </Flex>
+      </Flex>,
+    )
+  }
+
+  if (error) {
+    return layout(
+      <Router>
+        <Route default component={$error} />
+      </Router>,
+    )
+  }
+
+  // Optimize this in a bit
+  if (!definitions.app || !definitions.app.path) {
+    return layout(
+      <div>Error: Invalid app configuration</div>,
+    )
+  }
+
+  return layout(
+    <Router>
+      <Route
+        path={definitions.app.path[0].path}
+        component={Home}
+      />
+      <Route
+        path={definitions.app.path[1].path}
+        component={Resume}
+      />
+      <Route
+        path={definitions.app.path[2].path}
+        component={Project}
+      />
+      <Route
+        path={definitions.app.path[3].path}
+        component={Work}
+      />
+      <Route
+        path={definitions.app.path[4].path}
+        component={Contact}
+      />
+      <Route default component={$error} />
+    </Router>,
   )
 }
 
