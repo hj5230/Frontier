a89acc52b824f7a8e4ea87c2c6dcd335258d4cc0
hj5230 <hf305@foxmail.com>
2024-09-22 08:25:56 +0800

    refactor: Remove unused code

--- src/services/constants.ts
+++
@@ -1,5 +0,0 @@
-export const RESTfulBaseURL = 'http://localhost:3000'
-export const SocketBaseURL = 'ws://localhost:3000'
-export const UploadURL = 'http://localhost:3000'
-export const DefinitionsURL =
-  'https://cdn.jsdelivr.net/gh/hj5230/assets/Frontier/definitions'
