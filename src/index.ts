391cd4b04c5ffe5adc21d351b798d616372136fd
hj5230 <hf305@foxmail.com>
2024-10-03 01:32:06 +0200

    refactor: refactor overall template and build with rsbuild

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import App from './app'
-import '@radix-ui/themes/styles.css'
-// import '@style/index.css'
-
-export default App
--- src/sw.js
+++
@@ -1,8 +0,0 @@
-import {
-  getFiles,
-  setupPrecaching,
-  setupRouting,
-} from 'preact-cli/sw/'
-
-setupRouting()
-setupPrecaching(getFiles())
